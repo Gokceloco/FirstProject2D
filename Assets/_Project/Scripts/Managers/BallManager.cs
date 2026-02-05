2af38f6fcef5642053e805ff6b1af1257b033828
gokcegun <gokce@locomotive.games>
2026-02-05 19:59:31 +0300

    GameDirector Added

--- Assets/_Project/Scripts/Managers/BallManager.cs
+++ Assets/_Project/Scripts/Managers/BallManager.cs
@@ -4,27 +4,5 @@ using UnityEngine.InputSystem;
 
 public class BallManager : MonoBehaviour
 {
-    public List<Ball> balls = new List<Ball>();
 
-    private void Update()
-    {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            var tempList = new List<Ball>(balls);
-            foreach (var b in tempList)
-            {
-                var newBall = Instantiate(b);
-                balls.Add(newBall);
-            }
-        }
-
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-        {
-            foreach (var b in balls)
-            {
-                Destroy(b.gameObject);
-            }
-            balls.Clear();
-        }
-    }
 }
